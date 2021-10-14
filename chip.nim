1ac1871f1258bfb2bbd6ebeb86f34f6ddf2576c4
Nathan Kozyra <nkozyra@gmail.com>
2021-10-13 23:23:14 -0400

    Split logfile by lines.

--- chip.nim
+++ chip.nim
@@ -1,9 +1,17 @@
 import os
+import re
 import system
 
 if paramCount() == 0:
     stderr.writeLine("error: missing log file")
     quit(0)
 
-let file = readFile(paramStr(1))
-echo file
+var file = ""
+try:
+    file = readFile(paramStr(1))
+except IOError as e:
+    stderr.writeLine("error: ",e.msg)
+    quit(0)
+
+let lines = split(file, re"\n")
+echo lines
